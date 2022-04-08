a58cad110bdcb63290b64ed4ec625c6040cd3324
Omar Medjaouri <omedjaouri@gmail.com>
2022-04-08 16:55:32 -0500

    Added login from other UI and disabled the Nav bar for now

--- src/App.js
+++ src/App.js
@@ -2,11 +2,10 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Login from './components/pages/Login'
+import Login from './components/login/Login'
 import Photos from './components/pages/Photos'
 import Groups from './components/pages/Groups'
 import GroupPage from './components/pages/GroupPage'
-
 import Messages from './components/pages/Messages'
 import Contact from './components/pages/Contact'
 import Home from './components/pages/Home'
@@ -15,20 +14,27 @@ import Home from './components/pages/Home'
 function App() {
   return (
     <>
-  
+    <Router>
+        <Routes>
+            <Route exact path='/' element={<Login/>}/>
+        </Routes>
+    </Router>
+    
+
+    /*
     <Router>
     <Navbar />
       <Routes>
-        <Route exact  path='/' element={<Home />}/>
+        <Route exact  path='/' element={<Login />}/>
         <Route exact path='/login'  element={<Login />}/>
         <Route exact path='/photos'  element={<Photos />}/>
         <Route exact path='/groups'  element={<Groups />}/>
         <Route exact path='/groupPage'  element={<GroupPage />}/>
         <Route exact path='/messages'  element={<Messages />}/>
         <Route exact path='/contact'  element={<Contact />}/>
-
       </Routes>
     </Router>
+    */
     </>
   );
 }
---
+++ src/components/login/Login.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import mockData, { existingUsers } from '../../helper/mockData';
+import stickmanLogo from '../../images/stickmanLogo.png';
+import '../../css/Login.css'
+import LoginStandard from './LoginStandard';
+import LoginSignup from './LoginSignup';
+import LoginForgot from './LoginForgot';
+
+
+const Login = (props) => {
+
+    const [state, setState] = useState({
+        index : 0
+    })
+
+    const updateView = (i) => {
+            console.log("UPDATE VIEW INDEX TO : ",i)
+            setState(
+                {
+                    index: i      
+                })
+    }
+   
+    let displayedPage 
+    if(state.index === 0){
+        console.log("STATE IN PARENT IS : ",state.index)
+        displayedPage = (<LoginStandard handler={props.handler} viewUpdate={updateView}/>);
+    }
+    else if (state.index === 1){
+        console.log("STATE IN PARENT IS : ",state.index)
+        displayedPage = (<LoginSignup viewUpdate={updateView}/>);
+    }else if (state.index === 2){
+        console.log("STATE IN PARENT IS : ",state.index)
+        displayedPage = (<LoginForgot viewUpdate={updateView}/>);
+    }
+
+	return(
+    <>
+       {displayedPage}
+    </>
+    );
+};
+
+export default Login;
