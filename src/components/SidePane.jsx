a08a9c225800a8d05829d2796c122c44b8c19390
Cameron <camcamfresh@gmail.com>
2022-04-10 23:45:07 -0500

    add li tags and update aria for sidepane

--- src/components/SidePane.jsx
+++ src/components/SidePane.jsx
@@ -1,36 +1,44 @@
-import React from "react";
-import "../css/SidePane.css";
-import {mockData, getDataMap}from "../helper/mockData";
-import TripItem from "./TripItem";
-import profileImage from "../images/user.png";
-import jsCookie from "js-cookie";
+import React from 'react';
+import '../css/SidePane.css';
+import { mockData } from '../helper/mockData';
+import TripItem from './TripItem';
+import profileImage from '../images/user.png';
+import jsCookie from 'js-cookie';
 
 const SidePane = () => {
-  const user = jsCookie.get('username');
-  let tripList = mockData.users[user].trips.map(tripId => ({id: tripId, ...mockData.trips[tripId]}));
-  return (
-    <>
-      <div className="sidePaneContainer">
-        <div className="profileSection">
-          <div className="proPicContainer">
-            <img
-              className="proPic"
-              src={profileImage}
-              alt={"profile picture"}
-            />
-          </div>
-          <a href="" className="profileLink">
-            Your profile
-          </a>
-        </div>
-        <ul className="tripList">
-          {Array.from(tripList.values()).map((trip) => (
-            <TripItem key={trip.name} tripName={trip.name} tripId={trip.id}></TripItem>
-          ))}
-        </ul>
-      </div>
-    </>
-  );
+	const user = jsCookie.get('username');
+
+	let tripList = mockData.users[user].trips.map((tripId) => ({
+		id: tripId,
+		...mockData.trips[tripId],
+	}));
+
+	return (
+		<>
+			<div className='sidePaneContainer'>
+				<div className='profileSection'>
+					<div className='proPicContainer'>
+						<img
+							className='proPic'
+							src={profileImage}
+							alt={'my profile picture'}
+						/>
+					</div>
+					<a role='link' href='' className='profileLink'>
+						My profile
+					</a>
+				</div>
+				<ul className='tripList' aria-labelledby='myTrips' aria-label='My Trips' tabIndex={0}>
+					<div className='text-center h4'>
+						<label id='myTrips'>My Trips</label>
+					</div>
+					{tripList.map((trip) => (
+						<TripItem key={trip.name} tripName={trip.name} tripId={trip.id} />
+					))}
+				</ul>
+			</div>
+		</>
+	);
 };
 
 export default SidePane;
