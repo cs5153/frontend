6bcb3827e4143295a36f24addf31b589d9001305
Cameron <camcamfresh@gmail.com>
2022-04-11 02:32:51 -0500

    retain mock data in cookies

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,11 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import UserProvider from './components/UserProvider';
 import Main from './components/Main';
 import './App.css';
+import { initMockData } from './helper/mockData';
 
 const App = () => {
+	initMockData();
+
 	return (
 		<BrowserRouter>
 			<UserProvider>
